45909875515cb363e7ff34a1ca273e7c84854769
SokyranTheDragon <exploder1531@gmail.com>
2024-04-09 21:47:04 +0200

    Added a patch to AttachableThing to offset its position with the pawn

---
+++ Source/HarmonyPatches/OffsetAttachableThing.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+using YayoAnimation.Data;
+
+namespace YayoAnimation.HarmonyPatches;
+
+[HarmonyPatch(typeof(AttachableThing), nameof(AttachableThing.DrawPos), MethodType.Getter)]
+public static class OffsetAttachableThing
+{
+    public static void Postfix(ref Vector3 __result, Thing ___parent)
+    {
+        if (___parent is Pawn pawn)
+            __result += pawn.GetData().posOffset;
+    }
+}
